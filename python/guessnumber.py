eb5fa4fcb5008eda66a51f03f16617588f56b468
Dinoi <architectstan01@gmail.com>
2023-01-01 12:56:09 +0100

    guessing

--- python/guessnumber.py
+++ python/guessnumber.py
@@ -1,8 +1,16 @@
 import random
-
-se.number = random.randint(1, 20)
+import sys
+secretnumber = random.randint(1, 20)
 print('Thinking of a number between 1 to 20')
 
 for guessinput in range(1, 8):
     print('Take a guess')
     guess = int(input())
+
+    if guess < secretnumber:
+        print('Guess too low')
+    elif guess > secretnumber:
+        print('Guess too high')
+    else:
+        print('you got it dumbass')
+        sys.exit()
